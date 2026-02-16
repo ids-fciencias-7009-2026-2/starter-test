ec54d6dedf715bf27c4d849cf51fb558319bd715
md_cess <mdcesar136@gmail.com>
2026-02-16 15:17:27 -0600

    Practica 0 - Setup y diagnóstico funcional

---
+++ src/main/kotlin/com/ids/starter/test/SystemDiagnosticsByCesarFuentes.kt
@@ -0,0 +1,34 @@
+package com.ids.starter.test
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.boot.CommandLineRunner
+
+@Component
+class SystemDiagnosticsByCesarFuentes (
+    @Value("\${APP_NAME}") private val appName: String,
+    @Value("\${APP_ENV}") private val appEnv: String,
+    @Value("\${STUDENT_NAME}") private val studentName: String,
+    @Value("\${MAX_USERS}") private val maxUsers: Int,
+
+    ) : CommandLineRunner {
+    private val logger = LoggerFactory.getLogger(SystemDiagnosticsByCesarFuentes::class.java)
+
+    override fun run(vararg args: String) {
+        logger.info("Iniciando $appName")
+        logger.info("Entorno: $appEnv")
+        logger.info("Alumno: $studentName")
+        if (appEnv == "dev") {
+            logger.info("Modo desarrollo activo")
+        } else {
+            logger.info("Modo produccion activo")
+        }
+        if (maxUsers < 10) {
+            logger.warn("Capacidad maxima del sistema: $maxUsers usuarios")
+        } else {
+            logger.info("Capacidad maxima del sistema: $maxUsers usuarios")
+        }
+        logger.info("Sistema listo para iniciar correctamente")
+
+    }
+}
